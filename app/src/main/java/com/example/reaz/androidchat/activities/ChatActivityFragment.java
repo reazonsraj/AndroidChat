fc5bdc91d87b371d538695e35cbfd5067253b29b
reazonsraj <reazonsraj@gmail.com>
2017-08-23 17:24:53 +0545

    Initial commit

---
+++ app/src/main/java/com/example/reaz/androidchat/activities/ChatActivityFragment.java
@@ -0,0 +1,24 @@
+package com.example.reaz.androidchat.activities;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.reaz.androidchat.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ChatActivityFragment extends Fragment {
+
+    public ChatActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+}
